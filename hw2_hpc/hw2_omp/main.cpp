89d212ba6ba26aaedae5cb290173a74c15788c35
Han Tran <hantran@hantran.cs.utah.edu>
2020-03-02 13:09:14 -0700

    update

--- hw2_hpc/hw2_omp/main.cpp
+++ hw2_hpc/hw2_omp/main.cpp
@@ -10,10 +10,21 @@ const unsigned long  NUM=100000000;
 int * A = (int *)malloc(sizeof(int)*NUM);
 
 double t1= omp_get_wtime();
+
+std::cout << "==== Outside #pragma omp parallel ====\n";
+std::cout << "Number of threads can be used = " << omp_get_max_threads() << "\n";
+std::cout << "Number of threads currently being used = " << omp_get_num_threads() << "\n";
+std::cout << "\n";
+std::cout << "Start paralleling...\n";
+
 #pragma omp parallel
 {
+    std::cout << "Number of threads can be used = " << omp_get_max_threads() << "\n";
+    std::cout << "Number of threads currently being used = " << omp_get_num_threads() << "\n";
+    std::cout << "This is thread " << omp_get_thread_num() << "\n";
+
     int rank;
-    const int max=omp_get_max_threads();
+    const int max = omp_get_max_threads();
     rank = omp_get_thread_num(); // thread id
 
     // distribute the array  A to threads
@@ -24,7 +35,7 @@ double t1= omp_get_wtime();
         A[i] = rand() % 100; // generate random integers (0-99)
         //A[i] = 1;
     }
-};
+}
 double t2=omp_get_wtime();
 
 printf("Time taken: %.6fs\n", (t2-t1));
